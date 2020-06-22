3e95c2957c8991620cbb64765b09d2d4ff1e6f40
EC2 Default User <ec2-user@ip-172-31-90-45.ec2.internal>
2020-06-22 07:38:13 +0000

    web.phpを編集

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,44 @@
 Route::get('/', function () {
     return view('welcome');
     
-Route::resource('shop_items', 'shop_itemsController');
+
+
+//お店選択画面
+Route::get('shop_items', 'ShopItemController@index');
+
+Route::post('shop_items/{category_id}', 'ShopItemController@category');
+
+Route::get('shop_items/{shop_id}', 'ShopItemController@show');
+
+Route::PATCH('shop_items/{shop_id}', 'ShopItemController@toggleWant');
+
+Route::post('shop_items/{shop_id}', 'CommentController@store');
+
+
+
+//ルート作成画面
+Route::get('wants', 'WantController@index');
+
+Route::post('wants/{shop_id}', 'WantController@show');
+
+Route::delete('wants', 'WantController@destroy');
+
+Route::patch('wants', 'WantController@update');
+
+
+
+//口コミ投稿、閲覧画面
+Route::get('comments', 'CommentController@index');
+
+Route::post('comments', 'CommentController@store');
+
+
+
+//お問い合わせ画面
+Route::get('inquiries', 'InquiryController@index');
+
+ROute::post('inquiries', 'InquiryController@store');
+
 
 
 });
