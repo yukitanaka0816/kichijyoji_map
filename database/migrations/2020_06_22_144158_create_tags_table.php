d1799e8ae75e86a94ebabd71811bbb4f9e3c5a7c
EC2 Default User <ec2-user@ip-172-31-83-177.ec2.internal>
2020-06-22 05:56:18 +0000

    yamauchiniしました

---
+++ database/migrations/2020_06_22_144158_create_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedBigInteger('category_id');
+            
+            $table->foreign('shop_id')->references('id')->on('shop_items')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
