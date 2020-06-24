042541b3d54629d2ac001b4478109a1bc82feb32
EC2 Default User <ec2-user@ip-172-31-89-6.ec2.internal>
2020-06-24 07:27:03 +0000

    conflict解消

--- app/User.php
+++ app/User.php
@@ -38,13 +38,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
-<<<<<<< HEAD
     public function user_wants(){
         return $this->hasMany('App\Wants');
-=======
+    }
     //コメントとのリレーション
     public function comments() {
         return $this->hasOne('App\Comment');
->>>>>>> develop
     }
 }
